a670277775ccf240080bfac699bbe8a54f83a682
Abhinav Kumar <70753450+insane-abhinav@users.noreply.github.com>
2020-10-01 01:28:32 +0530

    Reduced its Complexity(i.e. Beginner-friendly)

--- palindrome_or_not_palindrome_python.py
+++ palindrome_or_not_palindrome_python.py
@@ -1,10 +1,8 @@
-string =  input()
-for i in range(0, int(len(string)/2)): 
-        if(string[i] != string[len(string)-i-1]):
-            ans=0
-        else:
-            ans=1
-if(ans==1):
-    print("Palindrome")
+#Palindrome or Not?
+s = input('Enter your string: ')
+s = s.lower()   #Make string lowercase if string contain uppercase
+
+if s[:]==s[::-1]:       #condition check      
+        print('Palindrome!')
 else:
-    print("Not Palindrome")
+        print('Not Palindrome!')
