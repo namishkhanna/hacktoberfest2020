1c90b1e54d3a4d1ecaf5913c33e6646b8c6af683
kulendu <imkulendu@gmail.com>
2020-10-05 04:52:32 +0530

    adding a palindrome file using list compreshension using py

---
+++ palindrome_using_math_func.py
@@ -0,0 +1,20 @@
+# checking a number is palindrome 
+import math 
+
+# the recursive function to reverse 
+def rev(num): 
+	return int(num != 0) and ((num % 10) * \ 
+			(10**int(math.log(num, 10))) + \ 
+						rev(num // 10)) 
+
+# initializing number 
+test_number = 9669669
+
+# printing the original number 
+print ("The original number is : " + str(test_number)) 
+
+# for checking a number is palindrome 
+res = test_number == rev(test_number) 
+
+# printing result 
+print ("Is the number palindrome ? : " + str(res)) 
