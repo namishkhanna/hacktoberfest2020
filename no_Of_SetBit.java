686e1eb8b0eb2ea290e1f7f2f7c6623162898a65
SHIVAM JAISWAL <39907285+shivamjaiswal64@users.noreply.github.com>
2020-10-05 22:40:00 +0530

    Create no_Of_SetBit java program

---
+++ no_Of_SetBit.java
@@ -0,0 +1,11 @@
+    /*
+    * Java program to find number of set bit using inbuilt-method
+    * @shivamjaiswal64
+    */
+    public class no_Of_SetBit{  
+        public static void main(String... args)  
+        {  
+            int num = 25; 
+            System.out.println(Integer.bitCount(num));  
+        }
+    } 
