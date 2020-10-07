42455ab7bd32b61379f585bcbe53cad10cd8add2
RAHUL SENAPATI <72217211+rahul-7077@users.noreply.github.com>
2020-10-07 20:15:03 +0530

    Save the prisoner
    A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
    
    The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. Here we'll determine the chair number occupied by the prisoner who will receive that candy.

---
+++ SaveThePrisoner.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+
+int saveThePrisoner(int n, int m, int s) {
+    if (n == m) {
+        if (s==1) return m;
+        else return s-1;
+    }
+    else if (n>m) {
+        if ((m+s-1) > n) return (m+s-1)%n;
+        else return m+s-1;
+    } else {
+        if (s==1 && m%n == 0) return n;
+        else if ((m%n)+s-1 > n) return ((m%n)+s-1)%n;
+        else return (m%n)+s-1;
+    }
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nms_temp;
+        getline(cin, nms_temp);
+
+        vector<string> nms = split_string(nms_temp);
+
+        int n = stoi(nms[0]);
+
+        int m = stoi(nms[1]);
+
+        int s = stoi(nms[2]);
+
+        int result = saveThePrisoner(n, m, s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
