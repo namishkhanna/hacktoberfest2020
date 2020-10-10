4268051f30a21296427247e591292c3fecea8d71
Rutuja Dicholkar <44153792+Rutujavd@users.noreply.github.com>
2020-10-10 20:19:36 +0530

    Add files via upload

---
+++ Rutujavd.py
@@ -0,0 +1,7 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hacktoberfest")
+Hacktoberfest
+>>> 2020+2020
+4040
+>>> 
