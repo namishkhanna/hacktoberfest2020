1afbc060a001d0da25d5de432d2fd837e7dad337
japmeet01 <72161668+japmeet01@users.noreply.github.com>
2020-10-15 02:22:35 +0530

    Create for_loop.c

---
+++ for_loop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	for(number=1;number<=10;number++)	//for loop to print 1-10 numbers
+	{
+		printf("%d\n",number);		//to print the number
+	}
+	return 0;
+}
