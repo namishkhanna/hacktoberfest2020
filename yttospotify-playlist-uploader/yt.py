7e78be753c63beb2735be0d81e71315ec3b43b93
Adrian <adrian_zinko@op.pl>
2020-10-18 19:47:34 +0200

    feat: implement init yt function

--- yttospotify-playlist-uploader/yt.py
+++ yttospotify-playlist-uploader/yt.py
@@ -1,12 +1,33 @@
 # Youtube management class
+import os
+
+import google_auth_oauthlib
+import googleapiclient
 
 class Youtube(object):
-  def __init_(self):
-    pass
+  def __init_(self, credentials_location):
+    scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        credentials_location, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    self.youtube_client = youtube_client
+
 
 def get_playlists(self):
   pass
-
+  
 def get_videos_from_playlist(self, playlist_id):
   pass
 
