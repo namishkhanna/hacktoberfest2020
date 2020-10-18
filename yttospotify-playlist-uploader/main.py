65c20d9da9b0e84313d163c2c0dba8e3ec6f4fe7
Adrian <adrian_zinko@op.pl>
2020-10-18 19:15:41 +0200

    feat: init setup

---
+++ yttospotify-playlist-uploader/main.py
@@ -0,0 +1,5 @@
+def run():
+  pass
+
+if __name__ == '__main__':
+  run()
---
+++ yttospotify-playlist-uploader/yt.py
@@ -0,0 +1,14 @@
+# Youtube management class
+
+class Youtube(object):
+  def __init_(self):
+    pass
+
+def get_playlists(self):
+  pass
+
+def get_videos_from_playlist(self, playlist_id):
+  pass
+
+def get_artist_and_track_from_video(self, video_id):
+  pass
