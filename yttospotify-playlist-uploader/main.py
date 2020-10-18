4cf9b8c2f77c333af554f194074b7c2542c37a66
Adrian <adrian_zinko@op.pl>
2020-10-18 21:47:02 +0200

    feat: add todo project requirements

--- yttospotify-playlist-uploader/main.py
+++ yttospotify-playlist-uploader/main.py
@@ -1,5 +1,10 @@
 def run():
   pass
+  # Get a list of playlists from yt
+  # Ask which playlist we want to get the music videos from
+  # For each video in the playlist get the song info from yt
+  # Search for desired song on Spotify
+  # Having found the desired song, add it to Spotify liked Songs
 
 if __name__ == '__main__':
   run()
