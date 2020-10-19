0f0b6f760b264d734b28f0f923a6afda48538fe6
naaz <ak3502186@gmail.com>
2020-10-19 10:24:45 +0530

    Added a python file containing program for finding solution of quadratic equation.

--- Quadratic solution.py
+++ Quadratic solution.py
@@ -4,5 +4,5 @@ def QuadS(a,b,c):
 		print("Real solution exist-")
 		print(((-b)+pow(d,0.5))/(2*a),((-b)-pow(d,0.5))/(2*a))
 	else:print("No real solution exist.")
-a,b,c=map(int,input('Enter the coeficients of x\u00b2 ,x and constant term-\n').split())
-QuadS(a,b,c)
+x,y,z=map(int,input('Enter the coeficients of x\u00b2 ,x and constant term-\n').split())
+QuadS(x,y,z)
