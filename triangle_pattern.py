eb866f5437362d21a2b9d20d0daf30bd2d54d539
ritishkhanna17 <73173658+ritishkhanna17@users.noreply.github.com>
2020-10-20 17:27:09 +0530

    Create triangle_pattern.py
    Triangle Printing in Python

---
+++ triangle_pattern.py
@@ -0,0 +1,11 @@
+def triangle(n): 
+    k = 2*n - 2  
+    for i in range(0, n): 
+        for j in range(0, k): 
+            print(end=" ") 
+        k = k - 1
+        for j in range(0, i+1): 
+            print("* ", end="") 
+        print("\r") 
+n = 10
+triangle(n) 
