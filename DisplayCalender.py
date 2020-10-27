1db4c61f957b1b1c3b6e60534b0975c2e00f7622
MAYANK CHAUDHARI <65117236+mayankcs@users.noreply.github.com>
2020-10-27 13:32:15 +0530

    Program to display calendar
    this is Program to display calendar of the given month and year .
    you can also take user input by simply uncommenting line 10 and 11 along with commenting line 6 and and 7 which will make you able to take user input for year and month

---
+++ DisplayCalender.py
@@ -0,0 +1,14 @@
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = 2020  # year
+mm = 10    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
