a9b893348f64e3056d0237e9bda0fe736a872e0e
Nihir Gupta <72348470+nihirgupta@users.noreply.github.com>
2020-10-29 23:23:54 +0530

    Create Checkarmstrongnumber.py

---
+++ Checkarmstrongnumber.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+   num2 = temp % 10
+   sum += num2 ** 3
+   temp //= 10
+
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
