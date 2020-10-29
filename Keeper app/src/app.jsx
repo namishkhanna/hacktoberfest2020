3cb88244fd7285af3058fc37081840f94c672ed4
Siddhesh Shinde <62283380+SiddheshShinde-tech@users.noreply.github.com>
2020-10-29 10:55:50 +0530

    Keeper Web App using React
    This file or folder is for HacktoberFest 2020. Submitted by Siddhesh Shinde.

---
+++ Keeper app/src/app.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "./Header";
+import Footer from "./Footer";
+import Mid from "./Note";
+
+
+function App(){
+    return (<div>
+    <Heading />
+    <Mid />
+    <Footer />   
+    </div>);
+}
+
+export default App;
