4240708ed349b8bbe83dbf34a546bd8a9234bb46
B Lekheshwar <66664839+Lekheshwar@users.noreply.github.com>
2020-11-03 11:48:51 +0530

    Create Minimum_Depth_of_Binary_Tree.cpp

---
+++ Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root)
+            return 0;
+        if (root-> left == NULL) {
+            return 1 + minDepth(root->right);
+        }
+        if (root -> right == NULL)
+            return 1 + minDepth(root->left);
+
+        return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
