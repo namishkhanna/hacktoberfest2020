6fa784f176581fc5766100727db28b4024f28680
Jai Yadav <90555918+Jaiyadav88@users.noreply.github.com>
2022-10-05 15:43:03 +0530

    Update Print_Elements_of_Array.java
    I created main method and input the array to print the elements by calling the function by making
    the object of the class

--- JAVA/Print_Elements_of_Array.java
+++ JAVA/Print_Elements_of_Array.java
@@ -1,9 +1,24 @@
+import java.util.*;
 class Solution {
     void printArray(int arr[], int n) {
         
         for(int i=0;i<n;i++){
             System.out.print(arr[i]+" ");
         }
+   
+    }//printing the elements of the Array.
     
-    }
-}
+    
+    public static void main()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int n=sc.nextInt();
+        System.out.println("Enter the elements of the Array");
+        int A[]=new int[n];
+        for(int i=0;i<n;i++)
+            A[i]=sc.nextInt();
+        Solution ob=new Solution();
+            ob.printArray(A,n);
+      }// Inputting the elements of the Array and calling the function using object of the class.
+ }
